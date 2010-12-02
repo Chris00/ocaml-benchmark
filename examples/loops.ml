035f2c4f4ec357a36107c96b4b7b9d9c4493f86e
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-12-02 17:38:46 +0000

    Test for/rec for array access
    git-svn-id: svn://scm.ocamlcore.org/svn/ocaml-benchmark/trunk@19 6b2cc328-5666-4133-919a-3bd285693efb

---
+++ examples/loops.ml
@@ -0,0 +1,36 @@
+(* $Id: loops.ml,v 1.1 2004-08-25 11:40:37 chris_77 Exp $ *)
+
+open Benchmark
+
+(* Test for the speed of recusion w.r.t. imperative loops to access
+   arrays fo floats. *)
+
+let rec_loop (a : float array) =
+  let rec loop i =
+    if i < Array.length a then begin
+      a.(i) <- a.(i) +. 1.;
+      loop (i + 1)
+    end in
+  loop 0
+
+let rec_loop2 (a : float array) =
+  let len = Array.length a in
+  let rec loop i =
+    if i < len then begin
+      a.(i) <- a.(i) +. 1.;
+      loop (i + 1)
+    end in
+  loop 0
+
+let for_loop (a : float array) =
+  for i = 0 to Array.length a - 1 do
+    a.(i) <- a.(i) +. 1.
+  done
+
+let () =
+  let a = Array.make 100 1. in
+  let res = throughputN ~repeat:5 1
+              [("rec", rec_loop, a);
+               ("rec2", rec_loop2, a);
+               ("for", for_loop, a); ] in
+  tabulate res
