252bf4159cc4ce10f5d4f7d9bc88bda730da7554
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2010-12-02 17:38:56 +0000

    New ocamlbuild targets for examples.  Updated INSTALL file.
    git-svn-id: svn://scm.ocamlcore.org/svn/ocaml-benchmark/trunk@38 6b2cc328-5666-4133-919a-3bd285693efb

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -2,6 +2,8 @@ open Ocamlbuild_plugin
 open Command
 ;;
 
+Options.make_links := false;;
+
 dispatch begin function
 | After_rules ->
     ocaml_lib "benchmark";
@@ -10,17 +12,21 @@ dispatch begin function
     flag ["link"; "program"; "ocaml"; "byte"; "use_pcre"] (S[A"pcre.cma"]);
     flag ["link"; "program"; "ocaml"; "native"; "use_pcre"] (S[A"pcre.cmxa"]);
 
-    let examples =
-      Array.fold_right begin fun f acc ->
-        if Pathname.get_extension f = "ml" then
-          ("examples" / Pathname.update_extension "native" f) :: acc
-        else
-          acc
-      end (Pathname.readdir "examples") [] in
-    rule "All examples"
-      ~prod:"examples.otarget"
-      ~deps:examples
-      (fun _ _ -> Nop);
+    let examples_rule ext =
+      let examples =
+        Array.fold_right begin fun f acc ->
+          if Pathname.get_extension f = "ml" then
+            ("examples" / Pathname.update_extension ext f) :: acc
+          else
+            acc
+        end (Pathname.readdir "examples") [] in
+      rule ("All examples " ^ ext)
+        ~prod:("examples." ^ ext)
+        ~deps:examples
+        (fun _ _ -> Nop) in
+
+    examples_rule "byte";
+    examples_rule "native";
 
 | _ -> ()
 end
