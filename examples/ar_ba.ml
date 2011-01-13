0d9e916afceb6801bbf7c439f02b19be9cd3f773
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-01-13 01:48:48 +0000

    Allow to compile examples not using pcre if it is not present.
    git-svn-id: svn://scm.ocamlcore.org/svn/ocaml-benchmark/trunk@57 6b2cc328-5666-4133-919a-3bd285693efb

--- examples/ar_ba.ml
+++ examples/ar_ba.ml
@@ -7,13 +7,13 @@ let n = 1000
 let a = Array1.create float64 fortran_layout n
 let () = Array1.fill a 1.
 
-let f1 () =
+let ba () =
   let s = ref 0. in
   for i = 1 to n do s := !s +. a.{i} done
 
 let b = Array.make n 1.
 
-let f2 () =
+let arr () =
   let s = ref 0. in
   for i = 0 to n-1 do s := !s +. b.(i) done
 
@@ -21,6 +21,6 @@ let f2 () =
 open Benchmark
 
 let () =
-  let res = throughputN ~repeat:5 3 [("ba", f1, ());
-                                     ("arr", f2, ()) ] in
+  let res = throughputN ~repeat:5 3 [("ba", ba, ());
+                                     ("arr", arr, ()) ] in
   tabulate res
