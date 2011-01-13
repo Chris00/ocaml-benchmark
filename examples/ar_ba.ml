9e2f6013554953564b85d55df75b003a7f8d950d
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-01-13 02:25:49 +0000

    Compare more ways of accessing (big)arrays.
    git-svn-id: svn://scm.ocamlcore.org/svn/ocaml-benchmark/trunk@58 6b2cc328-5666-4133-919a-3bd285693efb

--- examples/ar_ba.ml
+++ examples/ar_ba.ml
@@ -2,18 +2,37 @@
 
 open Bigarray
 
-let n = 1000
+let n = 10000
 
-let a = Array1.create float64 fortran_layout n
+type vec = (float, float64_elt, c_layout) Array1.t
+
+let a = Array1.create float64 c_layout n
 let () = Array1.fill a 1.
 
-let ba () =
+let ba (a: vec) =
   let s = ref 0. in
-  for i = 1 to n do s := !s +. a.{i} done
+  for i = 0 to n-1 do s := !s +. a.{i} done
+
+let ba_unbox (a: vec) =
+  let s = ref 0. in
+  for i = 0 to n-1 do s := !s +. a.{i} done;
+  !s *. 1.
+
+let ba_cl () =
+  let s = ref 0. in
+  for i = 0 to n-1 do s := !s +. a.{i} done
+
+let ba_gen a =
+  let s = ref 0. in
+  for i = 0 to n-1 do s := !s +. a.{i} done
 
 let b = Array.make n 1.
 
-let arr () =
+let arr (b: float array) =
+  let s = ref 0. in
+  for i = 0 to n-1 do s := !s +. b.(i) done
+
+let arr_cl () =
   let s = ref 0. in
   for i = 0 to n-1 do s := !s +. b.(i) done
 
@@ -21,6 +40,11 @@ let arr () =
 open Benchmark
 
 let () =
-  let res = throughputN ~repeat:5 3 [("ba", ba, ());
-                                     ("arr", arr, ()) ] in
+  let res = throughputN ~repeat:5 3
+    [("ba", (fun () -> ba a), ());
+     ("ba_unbox", (fun () -> ignore(ba_unbox a)), ());
+     ("ba_cl", ba_cl, ());
+     ("ba_gen", (fun () -> ba_gen a), ());
+     ("arr", (fun () -> arr b), ());
+     ("arr_cl", arr_cl, ()) ] in
   tabulate res
