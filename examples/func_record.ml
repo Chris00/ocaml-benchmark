4d51722edee714f620ed9782bdbcb60f87ac503c
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2011-08-21 08:22:31 +0000

    Comparison of various forms of parameterization.
    git-svn-id: svn://scm.ocamlcore.org/svn/ocaml-benchmark/trunk@76 6b2cc328-5666-4133-919a-3bd285693efb

---
+++ examples/func_record.ml
@@ -0,0 +1,39 @@
+module F(E : sig
+  val f : float -> float
+  val g : float -> float
+end) =
+struct
+  let h x = 1. +. E.f x +. E.g x
+end
+
+module A = F(struct let f x = x +. 1.  let g x = 2. *. x end)
+
+
+type env = { f : float -> float;  g : float -> float }
+
+let h_rec e x = 1. +. e.f x +. e.g x
+
+
+let h_fun f g x = 1. +. f x +. g x
+
+let f x = x +. 1.
+let g x = 2. *. x
+
+let h x = 1. +. f x +. g x
+
+
+open Benchmark
+
+let () =
+  let res = throughputN ~repeat:3 3
+    [("functor", (fun () -> A.h 1.), ());
+     ("record", (fun () -> h_rec { f = f; g = g } 1.), ());
+     ("fun arg", (fun () -> h_fun f g 1.), ());
+     ("no arg", (fun () -> h 1.), ());
+    ] in
+  print_endline "Functor vesus records vesus passing as arg:";
+  tabulate res
+
+(* Local Variables: *)
+(* compile-command: "make -k -C .." *)
+(* End: *)
